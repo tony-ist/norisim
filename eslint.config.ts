986d52d8f12462380e7b2b28bcb30bca392f48a1
Tony Istomin <istomanton@gmail.com>
2025-12-10 13:47:22 +0400

    chore: add stylistic

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,21 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import { defineConfig, globalIgnores } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
   { 
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
-    plugins: { js, react: pluginReact }, 
+    plugins: { js, react, stylistic }, 
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser }
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  react.configs.flat.recommended,
+  stylistic.configs.recommended,
   {
     rules: {
-      'react/react-in-jsx-scope': "off"
+      'react/react-in-jsx-scope': "off",
+      'stylistic/semi': ['error', 'always'],
     }
   },
   globalIgnores(["**/dist/**", "**/node_modules/**", '.eslintrc.cjs']),
