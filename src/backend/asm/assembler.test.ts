f2dca6b188407893e30952de92426a2a99846d51
Tony Istomin <istomanton@gmail.com>
2026-02-23 19:10:21 +0400

    chore: wip asm

--- src/backend/asm/assembler.test.ts
+++ src/backend/asm/assembler.test.ts
@@ -1,17 +1,47 @@
 import { describe, expect, it } from 'vitest';
-import { AST } from '../types/asm.types';
-import { assemble } from './assembler';
+import { AST, IRNode } from '../types/asm.types';
+import { assemble, encodeIRNode } from './assembler';
 
-describe('assemble', () => {
-  it('should assemble a simple program', () => {
-    const ast: AST = [
-      {
-        mnemonic: 'NOP',
-        forceUpdateFlags: false,
-        operands: [],
-      },
-    ];
-    const machineCode = assemble(ast);
-    expect(machineCode).toEqual([0x00]);
+describe('encodeIRNode', () => {
+  it('should encode Z format instruction', () => {
+    const irNode: IRNode = {
+      mnemonic: 'NOP',
+      format: 'Z',
+      operands: [],
+      address: 0,
+    };
+    expect(encodeIRNode(irNode)).toEqual(0);
+  });
+
+  it('should encode I format instruction', () => {
+    const irNode: IRNode = {
+      mnemonic: 'ADDI',
+      format: 'I',
+      operands: [{ type: 'register', value: 1 }, { type: 'immediate', value: 255 }],
+      address: 0,
+    };
+    expect(encodeIRNode(irNode)).toEqual(0b11111111_001_00010);
+  });
+
+  it('should encode A format instruction', () => {
+    const irNode: IRNode = {
+      mnemonic: 'ADD',
+      format: 'A',
+      operands: [{ type: 'register', value: 1 }, { type: 'register', value: 2 }, { type: 'register', value: 3 }],
+      address: 0,
+      forceUpdateFlags: true,
+    };
+    expect(encodeIRNode(irNode)).toEqual(0b10_001_010_011_00100);
+  });
+
+  it('should encode NAND', () => {
+    const irNode: IRNode = {
+      mnemonic: 'NAND',
+      format: 'A',
+      operands: [{ type: 'register', value: 1 }, { type: 'register', value: 2 }, { type: 'register', value: 3 }],
+      address: 0,
+      forceUpdateFlags: true,
+    };
+    expect(encodeIRNode(irNode)).toEqual(0b11_001_010_011_00100);
   });
 });
--- src/backend/asm/assembler.ts
+++ src/backend/asm/assembler.ts
@@ -1,20 +1,45 @@
-import { AST, ASTNode, REAL_INSTRUCTIONS } from '../types/asm.types';
+import { AST, IR, IRNode, Label, REAL_INSTRUCTIONS, RealInstructionMnemonic } from '../types/asm.types';
+import { compileToIR, generateIR } from './irgen';
+import { parseToAST } from './parser';
 
-function convertASTNodeToMachineCode(astNode: ASTNode): number[] {
-  const machineCode: number[] = [];
-  switch (astNode.mnemonic) {
-    case 'NOP':
-      machineCode.push(REAL_INSTRUCTIONS.NOP.opcode);
-      break;
+const INVERTED_MNEMONICS = new Set<RealInstructionMnemonic>(['NAND', 'NOR', 'XNOR']);
+
+export function encodeIRNode(irNode: IRNode): number {
+  const opcode = REAL_INSTRUCTIONS[irNode.mnemonic].opcode;
+
+  switch (irNode.format) {
+    case 'Z':
+      return opcode << 11;
+
+    case 'I': {
+      const register = irNode.operands[0].value as number;
+      const immediate = irNode.operands[1].value as number;
+      return immediate << 8 | register << 5 | opcode;
+    }
+
+    case 'A': {
+      const dest = irNode.operands[0].value as number;
+      const srcA = irNode.operands[1].value as number;
+      const srcB = irNode.operands[2].value as number;
+      const updateFlagsBit = irNode.forceUpdateFlags ? 1 : 0;
+      return updateFlagsBit << 15 | dest << 11 | srcA << 8 | srcB << 5 | opcode;
+    }
+
+    case 'B': {
+
+    }
+
+    case 'C': {
+
+    }
+
+    case 'J': {
+
+    }
   }
-  return machineCode;
 }
 
-export function assemble(ast: AST): number[] {
-  const machineCode: number[] = [];
-  for (const astNode of ast) {
-    const binary = convertASTNodeToMachineCode(astNode);
-    machineCode.push(...binary);
-  }
-  return machineCode;
+export function assemble(code: string): number[] {
+  const ir = compileToIR(code);
+  return ir.map(encodeIRNode);
 }
